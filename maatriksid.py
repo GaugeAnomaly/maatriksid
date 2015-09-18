5c75baa15222401f458d0614d7c0b30766cadb55
Jan <van_gentjanaare@hotmail.com>
2015-09-18 20:21:53 +0300

    Tegin koodi lyhemaks

--- maatriksid.py
+++ maatriksid.py
@@ -1,8 +1,4 @@
 #encoding: utf-8
-#Selle muudatuse tegin Code-is
-#selle ka
-#ja selle
-#
 def skalaar(v1,v2):
     return sum([v1[x]*v2[x] for x in range(len(v1))])
 
@@ -59,11 +55,9 @@ class Maatriks:
     def det(self):
         n = self.read_arv()
         if n != self.veer_arv():
-            print("Ei ole ruutmaatriks!!!")
-            return 0
+            return print("Ei ole ruutmaatriks!!!")
         if n == 0:
-            print("Maatris on tyhi!!!!")
-            return 0
+            return print("Maatris on tyhi!!!!")
         if n == 1:
             return self.M[0][0]
         return sum([self.M[0][i]*self.alamdet(0,i) for i in range(n)])
@@ -71,15 +65,13 @@ class Maatriks:
     def pöörd(self):
         d = self.det()
         if d == 0:
-            print("Sellel maatriksil ei saa olla pöördväärtust!!!!")
-            return
+            return print("Sellel maatriksil ei saa olla pöördväärtust!!!!")
         return self.transpoos().adj()*(1/d)
 
     def __mul__(self, m2):
         if type(m2) is Maatriks:
             if self.veer_arv() != m2.read_arv():
-                print("Neid maatrikseid ei saa korrutada!!!!")
-                return
+                return print("Neid maatrikseid ei saa korrutada!!!!")
             return fillMatrix(self.read_arv(),m2.veer_arv(),lambda x,y: int(round(skalaar(self.vec(x),m2.transpoos().vec(y)),3)))
         if type(m2) is int or type(m2) is float:
             return fillMatrix(self.read_arv(),self.veer_arv(),lambda x,y: self.M[x][y]*m2)
@@ -87,24 +79,20 @@ class Maatriks:
     def __rmul__(self, m2):
         if type(m2) is Maatriks:
             if self.read_arv() != m2.veer_arv():
-                print("Neid maatrikseid ei saa korrutada!!!!")
-                return
+                return print("Neid maatrikseid ei saa korrutada!!!!")
             return fillMatrix(m2.read_arv(),self.veer_arv(),lambda x,y: int(round(skalaar(m2.vec(x),self.transpoos().vec(y)),3)))
         if type(m2) is int or type(m2) is float:
             return fillMatrix(self.read_arv(),self.veer_arv(),lambda x,y: self.M[x][y]*m2)
         
     def __div__(self,x):
         if type(x) is Maatriks:
-            print("Maatrikseid ei saa jagada!!!!!")
-            return
+            return print("Maatrikseid ei saa jagada!!!!!")
         if type(x) is int or type(x) is float:
             return fillMatrix(self.read_arv(),self.veer_arv(),lambda k,l: self.M[k][l]/x)
 
     def __add__(self,m2):
         if type(m2) is int or type(m2) is float:
-            print("Sa ei saa maatrikseid ja numbreid liita")
-            return
+            return print("Sa ei saa maatrikseid ja numbreid liita")
         if self.read_arv() != m2.read_arv() or self.veer_arv() != m2.veer_arv():
-            print("Neid maatrikseid ei saa liita!!!!")
-            return
+            return print("Neid maatrikseid ei saa liita!!!!")
         return fillMatrix(self.read_arv(),self.veer_arv(),lambda x,y: self.M[x][y]+m2.M[x][y])
